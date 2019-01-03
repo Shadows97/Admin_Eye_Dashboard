95d12a19bf5b9640758660a47f6eabb132b88530
charbel <helpmoney97@gmail.com>
2019-01-03 12:29:30 +0100

    mise au point du systeme de session

--- Dashboard/models.py
+++ Dashboard/models.py
@@ -7,3 +7,17 @@ class Utilisateur(models.Model) :
     password = models.CharField(max_length=20)
     role = models.CharField(max_length=50)
     email = models.EmailField()
+
+
+class Moniteur(models.Model):
+    nom = models.CharField(max_length=50)
+    version = models.CharField(max_length=50)
+    utilisateur = models.ForeignKey(Utilisateur,on_delete=models.CASCADE)
+
+class Equipement(models.Model) :
+    nom = models.CharField(max_length=50)
+    adress_ip = models.CharField(max_length=50)
+    adress_mac = models.CharField(max_length=50)
+    etat = models.BooleanField(default=False)
+
+
--- Dashboard/urls.py
+++ Dashboard/urls.py
@@ -6,8 +6,7 @@ app_name="dashborad"
 urlpatterns = [
     path('login/', views.login,name='login'),
     path('register/', views.register, name='registe'),
-    path('registrer/do',views.doRegistry,name='doRegistry'),
-    path('index/',views.index,name='index'),
-    path('login/do', views.doLogin, name='doLogin'),
+    path('',views.index,name='index'),
+    path('logout/', views.logout, name='logout'),
 
 ]
